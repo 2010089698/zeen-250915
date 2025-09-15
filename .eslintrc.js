04d0da2db1c9b9ed68a10456d72402d9b23a637c
Naoya Togami <naoya.togami@colife.or.jp>
2025-09-15 14:39:12 +0900

    step 2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,25 @@
 module.exports = {
   extends: ['expo', '@typescript-eslint'],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'boundaries'],
+  settings: {
+    'import/resolver': {
+      'typescript': true
+    }
+  },
   rules: {
     '@typescript-eslint/no-unused-vars': 'error',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'warn',
+    'boundaries/element-types': [2, {
+      'default': 'disallow',
+      'rules': [
+        { 'from': ['app'], 'allow': ['features', 'config'] },
+        { 'from': ['features'], 'allow': ['domain', 'ports', 'schema', 'config'] },
+        { 'from': ['domain'], 'allow': ['schema'] },
+        { 'from': ['infra'], 'allow': ['ports', 'config', 'schema'] }
+      ]
+    }]
   },
 };
